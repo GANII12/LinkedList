5dd87bce9cf1674f03901696189db2ba9bcdd851
GANII12 <ganesh.17je003174@ece.ism.ac.in>
2022-02-23 12:29:39 +0530

    LinkedList&Stacks&Queues

---
+++ src/main/java/MyLinkedListTest.java
@@ -0,0 +1,59 @@
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class MyLinkedListTest {
+    @Test
+    public void given3NumbersWhenAddedToLinkedListShouldAddedToTop() {
+        MyNode<Integer> myFirstNode = new MyNode<>(70);
+        MyNode<Integer> mySecondNode = new MyNode<>(30);
+        MyNode<Integer> myThirdNode = new MyNode<>(56);
+        MyLinkedList myLinkedList = new MyLinkedList();
+        myLinkedList.add(myFirstNode);
+        myLinkedList.add(mySecondNode);
+        myLinkedList.add(myThirdNode);
+        myLinkedList.printMyNodes();
+
+        boolean result = myLinkedList.head.equals(myThirdNode) &&
+                myLinkedList.head.getNext().equals(mySecondNode) &&
+                myLinkedList.tail.equals(myFirstNode);
+
+        Assert.assertTrue(result);
+    }
+
+    //UC 3
+    @Test
+    public void given3NumbersWhenAppendedShouldBeAddedToLast(){
+        MyNode<Integer> myFirstNode = new MyNode<>(56);
+        MyNode<Integer> mySecondNode = new MyNode<>(30);
+        MyNode<Integer> myThirdNode = new MyNode<>(70);
+        MyLinkedList myLinkedList = new MyLinkedList();
+        myLinkedList.add(myFirstNode);
+        myLinkedList.append(mySecondNode);
+        myLinkedList.append(myThirdNode);
+        myLinkedList.printMyNodes();
+
+        boolean result = myLinkedList.head.equals(myFirstNode) &&
+                myLinkedList.head.getNext().equals(mySecondNode) &&
+                myLinkedList.tail.equals(myThirdNode);
+
+        Assert.assertTrue(result);
+    }
+
+    @Test
+    public void given3NumbersWhenInsertingSecondInBetweenShouldPassLinkedListResult(){
+        MyNode<Integer> myFirstNode = new MyNode<>(56);
+        MyNode<Integer> mySecondNode = new MyNode<>(30);
+        MyNode<Integer> myThirdNode = new MyNode<>(70);
+        MyLinkedList myLinkedList = new MyLinkedList();
+        myLinkedList.add(myFirstNode);
+        myLinkedList.append(myThirdNode);
+        myLinkedList.insert(myFirstNode,mySecondNode);
+
+        boolean result = myLinkedList.head.equals(myFirstNode) &&
+                myLinkedList.head.getNext().equals(mySecondNode) &&
+                myLinkedList.tail.equals(myThirdNode);
+
+        Assert.assertTrue(result);
+    }
+
+}
